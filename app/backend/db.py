ef7ed3b49f4a6b364bc9fbec06293d294312de32
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-12 20:01:00 +0300

    Создал базовый класс>

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine("sqlite:///ecommerce.db")
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
