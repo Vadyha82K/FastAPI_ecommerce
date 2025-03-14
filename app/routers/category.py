50d79e8a124811194248beb4e1f1bcc1f55e5671
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-14 11:04:28 +0300

    Создал ручку пост запроса для категорий и установил модуль slugify

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, status, Depends
+
+from app.backend.db_depends import get_db
+from app.models import Category
+from app.schemas import CreateCategory
 
 router = APIRouter(prefix="/categories", tags=["category"])
 
@@ -8,9 +15,20 @@ async def get_all_categories():
     pass
 
 
-@router.post("/")
-async def create_category():
+def slugify(name):
     pass
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
+    db.execute(insert(Category).values(name=create_category.name,
+                                       parent_id=create_category.parent_id,
+                                       slug=slugify(create_category.name)))
+    db.commit()
+    return {
+        "status_code": status.HTTP_201_CREATED,
+        "transaction": "Successful"
+    }
 
 
 @router.put("/")
