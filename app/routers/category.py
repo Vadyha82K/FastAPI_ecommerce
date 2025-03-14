8d18db44f7d4da0e2ce815653b05caecbab2ee25
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-14 16:17:57 +0300

    Создал ручки CRUD для категорий. Дополнил БД

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,7 +1,9 @@
+
 from typing import Annotated
-from sqlalchemy import insert
+from sqlalchemy import insert, select, update
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
+from slugify import slugify
 
 from app.backend.db_depends import get_db
 from app.models import Category
@@ -11,12 +13,9 @@ router = APIRouter(prefix="/categories", tags=["category"])
 
 
 @router.get("/")
-async def get_all_categories():
-    pass
-
-
-def slugify(name):
-    pass
+async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
+    categories = db.scalars(select(Category).where(Category.is_active == True)).all()
+    return categories
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -31,11 +30,37 @@ async def create_category(db: Annotated[Session, Depends(get_db)], create_catego
     }
 
 
-@router.put("/")
-async def update_category():
-    pass
+@router.put("/{category_slug}")
+async def update_category(db: Annotated[Session, Depends(get_db)], category_slug: str, update_category: CreateCategory):
+    category = db.scalar(select(Category).where(Category.slug == category_slug))
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no category found"
+        )
+    db.execute(update(Category).where(Category.slug == category_slug).values(
+        name=update_category.name,
+        slug=slugify(update_category.name),
+        parent_id=update_category.parent_id
+    ))
+    db.commit()
+    return {
+        "status_code": status.HTTP_200_OK,
+        "transaction": "Category update is successful"
+    }
 
 
-@router.delete("/")
-async def delete_category():
-    pass
+@router.delete("/{category_slug}")
+async def delete_category(db: Annotated[Session, Depends(get_db)], category_slug: str):
+    category = db.scalar(select(Category).where(Category.slug == category_slug, Category.is_active == True))
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no category found"
+        )
+    db.execute(update(Category).where(Category.slug == category_slug).values(is_active=False))
+    db.commit()
+    return {
+        "status_code": status.HTTP_200_OK,
+        "transaction": "Category delete is successful"
+    }
