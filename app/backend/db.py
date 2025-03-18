c5eb30a1d65af0deea163fc5c8157b510d3ffbbb
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-18 11:49:03 +0300

    Прописал асинхронный движок, создал новые миграции, удалив старые

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_async_engine('postgresql+asyncpg://ecommerce:postgres@localhost:5432/ecommerce', echo=True)
+engine = create_async_engine('postgresql+asyncpg://ecommerce:1234@localhost:5432/ecommerce', echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
