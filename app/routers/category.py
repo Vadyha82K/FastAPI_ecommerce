9b589eebd41bd5ee012e01d624e4080bfd338abe
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-18 18:09:42 +0300

    Перевел ручки категорий на асинхрон

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,9 +1,11 @@
 
 from typing import Annotated
 from sqlalchemy import insert, select, update
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, status, Depends, HTTPException
 from slugify import slugify
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.backend.db_depends import get_db
 from app.models import Category
@@ -12,18 +14,18 @@ from app.schemas import CreateCategory
 router = APIRouter(prefix="/categories", tags=["category"])
 
 
-@router.get("/")
-async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
-    categories = db.scalars(select(Category).where(Category.is_active == True)).all()
-    return categories
-
+@router.get('/')
+async def get_all_categories(db: Annotated[AsyncSession, Depends(get_db)]):
+    categories = await db.scalars(select(Category).where(Category.is_active == True))
+    return categories.all()
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
-    db.execute(insert(Category).values(name=create_category.name,
+async def create_category(db: Annotated[AsyncSession, Depends(get_db)], create_category: CreateCategory):
+    await db.execute(insert(Category).values(name=create_category.name,
                                        parent_id=create_category.parent_id,
                                        slug=slugify(create_category.name)))
-    db.commit()
+    await db.commit()
+
     return {
         "status_code": status.HTTP_201_CREATED,
         "transaction": "Successful"
@@ -31,19 +33,18 @@ async def create_category(db: Annotated[Session, Depends(get_db)], create_catego
 
 
 @router.put("/{category_slug}")
-async def update_category(db: Annotated[Session, Depends(get_db)], category_slug: str, update_category: CreateCategory):
-    category = db.scalar(select(Category).where(Category.slug == category_slug))
+async def update_category(db: Annotated[AsyncSession, Depends(get_db)], category_slug: str, update_category: CreateCategory):
+    category = await db.scalar(select(Category).where(Category.slug == category_slug))
     if category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="There is no category found"
         )
-    db.execute(update(Category).where(Category.slug == category_slug).values(
-        name=update_category.name,
-        slug=slugify(update_category.name),
-        parent_id=update_category.parent_id
-    ))
-    db.commit()
+        category.name = update_category.name,
+        category.slug = slugify(update_category.name),
+        category.parent_id = update_category.parent_id
+    await db.commit()
+
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Category update is successful"
@@ -51,15 +52,16 @@ async def update_category(db: Annotated[Session, Depends(get_db)], category_slug
 
 
 @router.delete("/{category_slug}")
-async def delete_category(db: Annotated[Session, Depends(get_db)], category_slug: str):
-    category = db.scalar(select(Category).where(Category.slug == category_slug, Category.is_active == True))
+async def delete_category(db: Annotated[AsyncSession, Depends(get_db)], category_slug: str):
+    category = await db.scalar(select(Category).where(Category.slug == category_slug, Category.is_active == True))
     if category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="There is no category found"
         )
-    db.execute(update(Category).where(Category.slug == category_slug).values(is_active=False))
-    db.commit()
+    category.is_active = False
+    await db.commit()
+
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Category delete is successful"
