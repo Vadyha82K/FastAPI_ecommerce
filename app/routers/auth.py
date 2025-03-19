550e4169c159142d2bbf9d59c17b20cd648a52cf
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-03-19 18:21:10 +0300

    Создал базовую аутентификацию

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from passlib.context import CryptContext
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.backend.db_depends import get_db
@@ -11,6 +12,23 @@ from app.schemas import CreateUser
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+security = HTTPBasic()
+
+
+async def get_current_username(
+        db: Annotated[AsyncSession, Depends(get_db)],
+        credentials: HTTPBasicCredentials = Depends(security)):
+    user = await db.scalar(select(User).where(User.username == credentials.username))
+    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    return user
+
+
+@router.get("/users/me")
+async def read_current_user(user: dict = Depends(get_current_username)):
+    return {"User": user}
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
